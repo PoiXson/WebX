7425272536079bbb74999a71d8f11efb0cd195d6
Pete Lorenzo <lorenzo@poixson.com>
2014-07-23 02:50:24 -0400

    added mostly empty WebX class

---
+++ java/src/com/poixson/webx/WebX.java
@@ -0,0 +1,31 @@
+package com.poixson.webx;
+
+import com.poixson.webx.bukkit.pxnBukkitPlugin;
+
+
+public class WebX extends pxnBukkitPlugin {
+
+
+
+	public WebX() {
+		super("webx");
+	}
+
+
+
+	@Override
+	protected void InitPlugin() {
+	}
+	@Override
+	protected void StartPlugin() {
+	}
+	@Override
+	protected void StopPlugin() {
+	}
+	@Override
+	protected void ReloadPlugin() {
+	}
+
+
+
+}
